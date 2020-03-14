50600be7403e869b6eb0c67e4d973c1cc219daab
ArleneDcosta <arlenedcosta77@gmail.com>
2020-03-14 06:38:48 +0530

    new java app loop error

--- While.java
+++ While.java
@@ -2,7 +2,7 @@ class While {
     public static void main(String args[]){
          int i=20;
          while(i>1){
-              System.out.println(i);
+              System.out.printlnh(i);
               i--;
          }
     }
